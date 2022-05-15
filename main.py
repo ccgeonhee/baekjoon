870468358337955ae14c7f536a29d6a402dba7c9
ccgeonhee <cckgh98@naver.com>
2022-05-16 08:27:48 +0900

    1065

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import pprint
 
 
-def self_num(a):
-    _num = [i for i in range(10000)]
-    val = set()
-    for i in range(a):
-        a = str(i)
-        sum = 0
-        for i in range(len(a)):
-            sum += int(a[i])
-        val.add(int(a) + sum)
+def equivalency(a):
+    equivalency_arr = []
+    for i in range(1, a + 1):
+        num = str(i)
+        if len(num) <= 2:
+            equivalency_arr.append(num)
+        else:
+            num_arr = [num[i] for i in range(len(num))]
+            diff = int(num_arr[-1]) - int(num_arr[-2])
+            err = 0
+            for j in range(len(num_arr) - 1, 0, -1):
+                cur_diff = int(num_arr[j]) - int(num_arr[j - 1])
+                if diff != cur_diff:
+                    err = 1
+                else:
+                    pass
+            if err == 0:
+                equivalency_arr.append(num)
+    han_len = len(equivalency_arr)
+    print(han_len)
+    return han_len
 
-    for i in range(len(_num)):
-        if _num[i] not in val:
-            print(_num[i])
 
-
-self_num(10000)
+_num = int(input())
+equivalency(_num)
