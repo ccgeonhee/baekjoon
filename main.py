7e47e4df24b4a4d8dcb4f73f937d3702bee83de1
ccgeonhee <cckgh98@naver.com>
2022-05-16 16:25:49 +0900

    1978

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
-import pprint
+total_input = int(input())
+input_string = input()
 
+input_arr = input_string.split()
 
-def equivalency(a):
-    equivalency_arr = []
-    for i in range(1, a + 1):
-        num = str(i)
-        if len(num) <= 2:
-            equivalency_arr.append(num)
-        else:
-            num_arr = [num[i] for i in range(len(num))]
-            diff = int(num_arr[-1]) - int(num_arr[-2])
-            err = 0
-            for j in range(len(num_arr) - 1, 0, -1):
-                cur_diff = int(num_arr[j]) - int(num_arr[j - 1])
-                if diff != cur_diff:
-                    err = 1
-                else:
-                    pass
-            if err == 0:
-                equivalency_arr.append(num)
-    han_len = len(equivalency_arr)
-    print(han_len)
-    return han_len
+result = 0
 
+for num in input_arr:
+    num = int(num)
+    if num <= 1:
+        continue
+    else:
+        err = 0
+        for i in range(2, num):
+            if num % i == 0:
+                err = 1
+                break
+            else:
+                pass
+        if err == 0:
+            result += 1
 
-_num = int(input())
-equivalency(_num)
+print(result)
