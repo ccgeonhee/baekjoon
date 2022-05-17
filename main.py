2517ca6145b992f27f3e973fd0ca90425b92a867
ccgeonhee <cckgh98@naver.com>
2022-05-18 08:34:40 +0900

    1929

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-def microfactorization(num):
-    if num == 1:
-        return 0
-    for i in range(2, num + 1):
-        if num % i == 0:
-            print(i)
-            microfactorization(int(num / i))
-            break
+def prime_list(min_val, max_val):
+    sieve = [False] * 2 + [True] * (max_val+1)
+
+    m = int(max_val ** 0.5)
+    for i in range(2, m + 1):
+        if sieve[i]:
+            for j in range(i+i, max_val+1, i):
+                sieve[j] = False
+
+    return [i for i in range(min_val, max_val+1) if sieve[i]]
 
 
-num = int(input())
-microfactorization(num)
+input_str = input()
+min_val, max_val = int(input_str.split()[0]), int(input_str.split()[1])
+prime_num = prime_list(min_val, max_val)
+for num in prime_num:
+    print(num)
+
+
