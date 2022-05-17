cc07c297a6fb3cc6849d60505286351517dde398
ccgeonhee <cckgh98@naver.com>
2022-05-17 17:02:40 +0900

    11653

--- main.py
+++ main.py
@@ -1,30 +1,12 @@
-min_val = int(input())
-max_val = int(input())
+def microfactorization(num):
+    if num == 1:
+        return 0
+    for i in range(2, num + 1):
+        if num % i == 0:
+            print(i)
+            microfactorization(int(num / i))
+            break
 
-sum = 0
-min = 0
-is_first = 0
-for i in range(min_val, max_val+1):
-    if i <= 1:
-        continue
-    else:
-        err = 0
-        for j in range(2, i):
-            if i % j == 0:
-                err = 1
-                break
-            else:
-                pass
-        if err == 0:
-            sum += i
-            if is_first == 0:
-                min = i
-                is_first = 1
-            else:
-                pass
 
-if sum != 0 and min != 0:
-    print(sum)
-    print(min)
-else:
-    print(-1)
+num = int(input())
+microfactorization(num)
